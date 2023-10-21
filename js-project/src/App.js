59846aeeea3db232334c0235fd056cc2c8d1f03c
AlexEngo <a.o.engovatov@yandex.ru>
2023-10-21 21:33:29 +0400

    debug without JSX

--- js-project/src/App.js
+++ js-project/src/App.js
@@ -1,25 +1,42 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 const year = new Date().getFullYear();
 
 export const App = () => {
-	return (
-		<div class="App">
-			<header class="App-header">
-				<img src={logo} class="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					class="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{year}</div>
-			</header>
-		</div>
+	return /*#__PURE__*/ createElement(
+		'div',
+		{
+			class: 'App',
+		},
+		/*#__PURE__*/ createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			/*#__PURE__*/ createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			/*#__PURE__*/ createElement(
+				'p',
+				null,
+				'Edit ',
+				/*#__PURE__*/ createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			/*#__PURE__*/ createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			/*#__PURE__*/ createElement('div', null, year),
+		),
 	);
 };
